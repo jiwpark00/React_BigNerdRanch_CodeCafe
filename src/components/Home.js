30380fb1f77d114099bcf6eb4a44b80a9f6ab0b8
jiwpark00 <jiwpark00@gmail.com>
2023-09-29 21:58:12 -0400

    Finished chapter 6 - added resuability for proptypes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import Thumbnail from './Thumbnail';
 import { itemImages } from '../items';
 import './Home.css';
+import ItemType from '../types/item';
 
 function Home({ items }) {
   return (
@@ -18,16 +19,7 @@ function Home({ items }) {
 }
 
 Home.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      itemId: PropTypes.string.isRequired,
-      imageId: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      price: PropTypes.number.isRequired,
-      description: PropTypes.string,
-      salePrice: PropTypes.number,
-    }),
-  ).isRequired,
+  items: PropTypes.arrayOf(ItemType).isRequired,
 };
 
 export default Home;
