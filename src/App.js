3d5abf07784164ca985654f73df89c88194b9cc7
jiwpark00 <jiwpark00@gmail.com>
2023-09-29 23:01:48 -0400

    Began to configure a local server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import { items } from './items';
 
 function App() {
+  useEffect(() => {
+    axios.get('/api/items');
+  }, []);
+
   return (
     <div>
       <Header />
