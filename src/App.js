e7798947c866d4a6df7b7b7e3ed8c79c5d26090e
jiwpark00 <jiwpark00@gmail.com>
2023-12-22 15:09:20 -0500

    Finished conditional rendering ch 10

--- src/App.js
+++ src/App.js
@@ -24,14 +24,18 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/details" element={<Details items={items} />}>
-          <Route path=":id" element={<DetailItem />} />
-          <Route index element={<div>No Item Selected</div>} />
-        </Route>
-        <Route path="/" element={<Home items={items} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {items.length === 0
+        ? <div>Loading...</div>
+        : (
+          <Routes>
+            <Route path="/details" element={<Details items={items} />}>
+              <Route path=":id" element={<DetailItem items={items} />} />
+              <Route index element={<div>No Item Selected</div>} />
+            </Route>
+            <Route path="/" element={<Home items={items} />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        )}
     </Router>
   );
 }
