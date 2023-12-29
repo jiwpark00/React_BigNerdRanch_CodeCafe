d03b401f60bf29196c2a8d3532e0ce024c125e97
jiwpark00 <jiwpark00@gmail.com>
2023-12-29 12:29:11 -0500

    Now showing the list of Orders

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types';
 import ItemType from '../types/item';
 import './Orders.css';
 
-// eslint-disable-next-line no-unused-vars
 function Orders({ items }) {
-// eslint-disable-next-line no-unused-vars
   const [orders, setOrders] = useState([]);
 
   useEffect(
@@ -20,6 +18,42 @@ function Orders({ items }) {
   return (
     <div className="orders-component">
       <h2>Existing Orders</h2>
+      {orders.length === 0
+        ? <div>No Orders</div>
+        : orders.map((order) => (
+          <div className="order" key={order.id}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Customer</th>
+                  <th>ZIP Code</th>
+                  {order.phone && <th>Phone</th>}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{order.name}</td>
+                  <td>{order.zipCode}</td>
+                  {order.phone && <td>{order.phone}</td>}
+                </tr>
+              </tbody>
+              <thead>
+                <tr>
+                  <th>Quantity</th>
+                  <th>Item</th>
+                </tr>
+              </thead>
+              <tbody>
+                {order.items.map((item) => (
+                  <tr key={item.itemId}>
+                    <td>{item.quantity}</td>
+                    <td>{items.find((i) => i.itemId === item.itemId)?.title}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
     </div>
   );
 }
