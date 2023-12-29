adc26484f9eeae8e6c7fcab02ad72134a3b7f68b
jiwpark00 <jiwpark00@gmail.com>
2023-12-29 12:03:11 -0500

    Starting to work on the Orders page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import NotFound from './components/NotFound';
 import { cartReducer, CartTypes, initialCartState } from './reducers/cartReducer';
 import CurrentUserContext from './contexts/CurrentUserContext';
 import Login from './components/Login';
+import Orders from './components/Orders';
 
 const storageKey = 'cart';
 
@@ -82,6 +83,7 @@ function App() {
               </Route>
               <Route path="/" element={<Home items={items} />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/orders" element={<Orders items={items} />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           )}
